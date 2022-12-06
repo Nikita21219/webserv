9f57e6e9ba27d36e1df98da63dd47b94ee3fafec
Nikita Madorsky <madorskii@mail.ru>
2022-12-06 18:35:45 +0300

    after merge

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,29 +1,35 @@
 #include "webserv.h"
 
 int main(int argc, char **argv) {
-	std::vector<Parser> conf;
-	if (argc != 2) {
-		std::cerr << "wrong number of arguments!\n";
-		return 1;
-	}
-	if (get_conf(argv[1], conf))
-		return 1;
+    (void) argc;
+    (void) argv;
 
-    Server serv = Server(conf);
+	// std::vector<Parser> conf;
+	// if (argc != 2) {
+	// 	std::cerr << "wrong number of arguments!\n";
+	// 	return 1;
+	// }
+	// if (get_conf(argv[1], conf))
+	// 	return 1;
+    //
+
+    
+    Server serv = Server(std::vector<Parser>()); //TODO replace to conf
     serv.mainLoop();
+
 
 /*
 **	these tests should be run with configs/example file!
 */
 
-	std::cout << " -----some tests----- \n";
-	std::cout << "serv1 methods: " << conf[0].getServfield("methods") << '\n';
-	std::cout << "serv2 methods: " << conf[1].getServfield("methods") << '\n';
-	std::cout << "serv1 (location /hub) redirection: " << conf[0].getLocfield("/hub", "redirection") << '\n';
-	std::cout << "serv2 (location /directory/*.bla) cgi_pass: " << conf[1].getLocfield("/directory/*.bla", "cgi_pass") << '\n';
-	if (conf[0].getLocfield("/wrong_path", "methods") == NOT_FOUND)
-		std::cout << "serv1 (location /wrong_path) methods: wasn't wound!\n";
-	std::cout << " -----done!----- \n";
+	// std::cout << " -----some tests----- \n";
+	// std::cout << "serv1 methods: " << conf[0].getServfield("methods") << '\n';
+	// std::cout << "serv2 methods: " << conf[1].getServfield("methods") << '\n';
+	// std::cout << "serv1 (location /hub) redirection: " << conf[0].getLocfield("/hub", "redirection") << '\n';
+	// std::cout << "serv2 (location /directory/*.bla) cgi_pass: " << conf[1].getLocfield("/directory/*.bla", "cgi_pass") << '\n';
+	// if (conf[0].getLocfield("/wrong_path", "methods") == NOT_FOUND)
+	// 	std::cout << "serv1 (location /wrong_path) methods: wasn't wound!\n";
+	// std::cout << " -----done!----- \n";
 
 	return 0;
 }
